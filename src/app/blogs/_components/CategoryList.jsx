90471f9ef216c820e187c570f850481dd305d82f
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-03-12 15:07:00 +0330

    Develop "CategoryList" component

---
+++ src/app/blogs/_components/CategoryList.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+
+const listStyles =
+  "rounded p-2 pr-6 transition-colors duration-300 hover:bg-secondary-100 lg:ml-6";
+
+async function CategoryList() {
+  const res = await fetch("http://localhost:5000/api/category/list");
+  const {
+    data: { categories },
+  } = await res.json();
+
+  return (
+    <ul className="space-y-2">
+      <li className={listStyles}>
+        <Link href="/blogs">همه</Link>
+      </li>
+
+      {categories.map((category) => (
+        <li key={category._id} className={listStyles}>
+          <Link href={`/blogs/category/${category.slug}`}>
+            {category.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+export default CategoryList;
--- src/app/blogs/layout.jsx
+++ src/app/blogs/layout.jsx
@@ -1,18 +1,20 @@
+import CategoryList from "./_components/CategoryList";
+
 export const metadata = {
   title: "بلاگ ها",
 };
 
 function BlogsLayout({ children }) {
   return (
-    <div>
+    <div className="mb-12">
       <h1 className="mb-12 text-lg font-bold">لیست بلاگ ها</h1>
 
       <div className="grid grid-cols-12 gap-8">
-        <div className="col-span-12 space-y-4 text-secondary-500 lg:col-span-4 xl:col-span-5">
-          Category List
+        <div className="col-span-12 space-y-4 text-secondary-500 md:col-span-4 lg:col-span-3">
+          <CategoryList />
         </div>
 
-        <div className="col-span-12 lg:col-span-8 xl:col-span-7">
+        <div className="col-span-12 md:col-span-8 lg:col-span-9">
           {children}
         </div>
       </div>
