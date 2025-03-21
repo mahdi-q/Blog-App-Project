76920c20ab7c2c5379802411693d3d9510ff7f7e
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-03-21 17:52:18 +0330

    Develop "Signup" page form

---
+++ src/app/(Auth)/signup/page.jsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Button from "@/ui/Button";
+import RHFTextField from "@/ui/RHFTextField";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup
+  .object({
+    fullname: yup
+      .string()
+      .required("نام و نام خانوادگی الزامی است")
+      .min(5, "نام باید بیشتر از ۵ کارکتر باشد")
+      .max(30, "نام باید کمتر از ۳۰ کارکتر باشد"),
+    email: yup.string().required("ایمیل الزامی است").email("ایمیل نامعتبر است"),
+    password: yup.string().required("رمز عبور الزامی است"),
+  })
+  .required();
+
+function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <div className="form">
+      <h1 className="text-lg font-bold text-secondary-800">ثبت نام</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <RHFTextField
+          label="نام و نام خانوادگی"
+          name="fullname"
+          register={register}
+          errors={errors}
+          isRequired
+        />
+
+        <RHFTextField
+          label="ایمیل"
+          name="email"
+          register={register}
+          errors={errors}
+          isRequired
+          dir="ltr"
+        />
+
+        <RHFTextField
+          label="رمز عبور"
+          name="password"
+          register={register}
+          errors={errors}
+          isRequired
+          type="password"
+          dir="ltr"
+        />
+
+        <Button className="mt-2 font-bold">ثبت نام</Button>
+      </form>
+    </div>
+  );
+}
+export default Signup;
