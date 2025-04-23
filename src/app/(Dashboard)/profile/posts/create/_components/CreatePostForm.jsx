6f561366e38b6562d2b71de64f2e254e008a25a4
Mahdi-Ghasemi <ghasemi84mahdi@gmail.com>
2025-04-23 16:10:48 +0330

    Enhance CreatePostForm validation and layout; integrate error handling in FileInput component

--- src/app/(Dashboard)/profile/posts/create/_components/CreatePostForm.jsx
+++ src/app/(Dashboard)/profile/posts/create/_components/CreatePostForm.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useCategories } from "@/hooks/useCategories";
+import Button from "@/ui/Button";
 import ButtonIcon from "@/ui/ButtonIcon";
 import FileInput from "@/ui/FileInput";
 import RHFSelect from "@/ui/RHFSelect";
@@ -13,14 +14,39 @@ import { Controller, useForm } from "react-hook-form";
 import * as yup from "yup";
 
 const schema = yup.object({
-  title: yup.string().required("عنوان الزامی است"),
-  briefText: yup.string().required("خلاصه متن الزامی است"),
-  text: yup.string().required("متن الزامی است"),
+  title: yup
+    .string()
+    .required("عنوان الزامی است")
+    .min(5, "عنوان باید حداقل ۵ کاراکتر باشد"),
+  briefText: yup
+    .string()
+    .required("خلاصه متن الزامی است")
+    .min(10, "خلاصه متن باید حداقل ۱۰ کاراکتر باشد"),
+  text: yup
+    .string()
+    .required("متن الزامی است")
+    .min(10, "متن باید حداقل ۱۰ کاراکتر باشد"),
   slug: yup.string().required("اسلاگ الزامی است"),
   readingTime: yup
     .number()
+    .positive("زمان مطالعه باید عدد مثبت باشد")
+    .integer("زمان مطالعه باید عدد صحیح باشد")
     .typeError("زمان مطالعه باید فقط عدد باشد")
     .required("زمان مطالعه الزامی است"),
+  category: yup.string().required("دسته بندی الزامی است"),
+  coverImage: yup
+    .mixed()
+    .test("required", "کاور پست الزامی است", (value) => {
+      return value;
+    })
+    .test("fileSize", "حجم فایل باید کمتر از 2 مگابایت باشد", (value) => {
+      return value && value.size <= 2 * 1024 * 1024;
+    })
+    .test("fileType", "فرمت فایل نامعتبر است", (value) => {
+      return (
+        value && ["image/jpeg", "image/png", "image/jpg"].includes(value.type)
+      );
+    }),
 });
 
 function CreatePostForm() {
@@ -40,8 +66,13 @@ function CreatePostForm() {
 
   const { categories } = useCategories();
 
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="post__form">
+    <form className="post__form" onSubmit={handleSubmit(onSubmit)}>
+      {/* Title Input */}
       <RHFTextField
         name="title"
         label="عنوان"
@@ -50,6 +81,7 @@ function CreatePostForm() {
         isRequired
       />
 
+      {/* Brief Text Input */}
       <RHFTextField
         name="briefText"
         label="خلاصه متن"
@@ -58,6 +90,7 @@ function CreatePostForm() {
         isRequired
       />
 
+      {/* Text Input */}
       <RHFTextField
         name="text"
         label="متن"
@@ -66,6 +99,7 @@ function CreatePostForm() {
         isRequired
       />
 
+      {/* Slug Input */}
       <RHFTextField
         name="slug"
         label="اسلاگ"
@@ -74,6 +108,7 @@ function CreatePostForm() {
         isRequired
       />
 
+      {/* Reading Time Input */}
       <RHFTextField
         name="readingTime"
         label="زمان مطالعه"
@@ -83,6 +118,7 @@ function CreatePostForm() {
         isRequired
       />
 
+      {/* Category Select Input */}
       <RHFSelect
         name="category"
         label="دسته بندی"
@@ -92,6 +128,7 @@ function CreatePostForm() {
         options={categories}
       />
 
+      {/* Cover Post Input */}
       <Controller
         name="coverImage"
         control={control}
@@ -101,6 +138,7 @@ function CreatePostForm() {
             label="انتخاب کاور پست"
             name="coverPost"
             value={value?.fileName}
+            errors={errors}
             onChange={(event) => {
               const file = event.target.files[0];
               onChange(file);
@@ -112,6 +150,7 @@ function CreatePostForm() {
         )}
       />
 
+      {/* Showing Cover Post */}
       {coverImageUrl && (
         <div className="relative aspect-video overflow-hidden rounded-lg">
           <Image
@@ -133,6 +172,10 @@ function CreatePostForm() {
           </ButtonIcon>
         </div>
       )}
+
+      <Button className="lg:col-start-2 lg:row-start-5 lg:w-1/2 lg:justify-self-end">
+        تایید
+      </Button>
     </form>
   );
 }
