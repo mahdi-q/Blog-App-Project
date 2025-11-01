49d97783f1a41d8bc742ceac8d9e99b5deb9f757
mahdi-q <ghasemi84mahdi@gmail.com>
2025-11-01 18:54:02 +0330

    Fix the bug of auth pages

--- src/app/(Auth)/layout.jsx
+++ src/app/(Auth)/layout.jsx
@@ -1,7 +1,7 @@
 function AuthLayout({ children }) {
   return (
     <div className="my-20 flex items-center justify-center">
-      <div className="w-full max-w-md">{children}</div>
+      <div className="mx-5 w-full max-w-md">{children}</div>
     </div>
   );
 }
